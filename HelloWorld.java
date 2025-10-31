1d4bb7a8f83e3156d7458b4b00445386ea530256
kirankgowda2023 <142824574+kirankgowda2023@users.noreply.github.com>
2025-11-01 00:41:37 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, Java is working successfully! 👋");
+
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+
+        System.out.println("Let's test a small calculation:");
+        System.out.println(a + " + " + b + " = " + sum);
+
+        System.out.println("Program executed successfully ✅");
+    }
+}
