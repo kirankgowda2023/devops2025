91776829db41066ab1d049c00b5e8b2c8407d330
kirankgowda2023 <142824574+kirankgowda2023@users.noreply.github.com>
2025-11-01 17:57:13 +0530

    Simplify HelloWorld output to 'Hello, World!'
    Removed previous output and calculation logic.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,14 +1,6 @@
+// HelloWorld.java
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, Java is working successfully! 👋");
-
-        int a = 10;
-        int b = 20;
-        int sum = a + b;
-
-        System.out.println("Let's test a small calculation:");
-        System.out.println(a + " + " + b + " = " + sum);
-
-        System.out.println("Program executed successfully ✅");
+        System.out.println("Hello, World!");
     }
 }
